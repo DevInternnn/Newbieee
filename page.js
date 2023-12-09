5daf63cf029e5f90fd8d61e2ca1cc5063b7aff94
DevInternnn <153365225+DevInternnn@users.noreply.github.com>
2023-12-09 20:15:45 +0800

    Add files via upload

---
+++ page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+   <div className='flex justify-center'>
+    <h1 className='text-[#67F9F6]'>hi</h1>
+   </div>
+  )
+}
